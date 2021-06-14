4dd79814b02c33a59684c794c1fdc57b744403c1
youkim <42.4.youkim@gmail.com>
2021-06-14 18:44:08 +0900

    with bonus

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youkim <youkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/12 15:20:19 by youkim            #+#    #+#             */
+/*   Updated: 2021/06/14 18:09:46 by youkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	dstlen;
+	size_t	srclen;
+
+	i = 0;
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (dstsize < dstlen + 1)
+		return (srclen + dstsize);
+	while (src[i] && (dstlen + i + 1) < dstsize)
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = 0;
+	return (srclen + dstlen);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dst || !src)
+		return (0);
+	if (dstsize > 0)
+	{
+		while (src[i] && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	return (ft_strlen(src));
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ret;
+	size_t	i;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	ret = malloc((len + 1) * sizeof(char));
+	if (!ret)
+		return (0);
+	ret[len] = 0;
+	i = -1;
+	while (++i < len)
+		ret[i] = s1[i];
+	return (ret);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	lens1;
+	size_t	lens2;
+	char	*str;
+
+	if (!s1 && !s2)
+		return (0);
+	else if (!s1)
+		return (ft_strdup(s2));
+	else if (!s2)
+		return (ft_strdup(s1));
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	str = malloc((lens1 + lens2 + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, s1, lens1 + 1);
+	free((char *)s1);
+	ft_strlcat(str, s2, lens1 + lens2 + 1);
+	return (str);
+}
